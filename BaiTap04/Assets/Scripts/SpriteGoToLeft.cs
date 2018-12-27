69241f6da71aad87057e6e6556726a7ace5c1801
nguyenduc1222 <nguyenduc1222@gmail.com>
2018-12-27 18:05:37 +0700

    Add

---
+++ BaiTap04/Assets/Scripts/SpriteGoToLeft.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteGoToLeft : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    private float t = 0.0f;
+    private bool moving = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("d"))
+        {
+            rb.velocity = new Vector3(5, 0, 0);
+            moving = true;
+            Debug.Log("jump");
+        }
+
+        if (moving)
+        {
+            t = t + Time.deltaTime;
+            if (t > 1.0f)
+            {
+                Debug.Log(gameObject.transform.position.y + " : " + t);
+                t = 0.0f;
+            }
+        }
+    }
+
+}
---
+++ BaiTap04/Assets/Scripts/SpriteGoToLeftOrRight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteGoToLeftOrRight : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    private float t = 0.0f;
+    private bool moving = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("d"))
+        {
+            rb.velocity = new Vector3(5, 0, 0);
+            moving = true;
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            rb.velocity = new Vector3(-5, 0, 0);
+            moving = true;
+        }
+
+
+        if (moving)
+        {
+            t = t + Time.deltaTime;
+            if (t > 1.0f)
+            {
+                Debug.Log(gameObject.transform.position.y + " : " + t);
+                t = 0.0f;
+            }
+        }
+    }
+}
